4b3bbcbf60c12d49ee803e1b5dca0d6267a9b19e
ta.sahibgareev <ta.sahibgareev@tensor.ru>
2018-03-23 13:04:32 +0500

    exmo api test

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
 from urllib import request
 
+
+API_URL = 'https://api.exmo.com/v1/'
+GET_TRADES = 'trades/'
+GET_STAT = 'ticker/'
+
+# req_str = '?pair=BTC_USD'
+# headers = {'pair': 'BTC_USD'}
+#
+# req_result = request.Request(API_URL + API_METHOD+req_str)
+#
+# with request.urlopen(req_result) as response:
+#     api_answer = response.read()
+
+req_result = request.Request(API_URL + GET_STAT)
+with request.urlopen(req_result) as response:
+    api_answer = response.read()
+
+print(api_answer)
