3ca8379e995816bd3c6f0b709ba7c859a6d9b7d6
ta.sahibgareev <ta.sahibgareev@tensor.ru>
2018-04-28 16:31:20 +0500

    Add class ui

--- main_window.py
+++ main_window.py
@@ -1,16 +1,32 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 import menu_controller
+
+
+class TMainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        """
+
+        :return:
+        """
+        ok_button = QPushButton("OK")
+        cancel_button = QPushButton("Cancel")
+        v_box = QHBoxLayout()
+        v_box.addStretch()
+        v_box.addWidget(ok_button)
+        v_box.addWidget(cancel_button)
+        self.setGeometry(300, 300, 300, 300)
+        self.setLayout(v_box)
+        self.show()
+
 
 app = QApplication(sys.argv)
 
-main_window = QWidget()
-menu_controller.main_menu_create(main_window)
-
-main_window.setGeometry(300, 300, 300, 200)
-main_window.setWindowTitle('Tooltips 2')
-main_window.show()
-
+window = TMainWindow()
 
 sys.exit(app.exec_())
--- menu_controller.py
+++ menu_controller.py
@@ -1,29 +1,56 @@
 # import sys
-from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QPushButton, QWidget, QHBoxLayout, QVBoxLayout
 from PyQt5.QtCore import QCoreApplication
 
 
-def main_menu_create(hwnd):
-    """
-
-    :param hwnd:
-    :return:
-    """
-
-    btn = QPushButton('Button', hwnd)
-    btn.clicked.connect(refresh)
-    btn.setToolTip('This is a <b>QPushButton</b> widget')
-    # btn.resize(btn.sizeHint())
-    btn.move(50, 50)
 
 
-def refresh():
-    """
 
-    :return:
-    """
-    # if hwnd is not None:
-    #     btn = QPushButton('Button', hwnd)
-    #     btn.setToolTip('This is a <b>QPushButton</b> widget 2')
-    #     btn.resize(btn.sizeHint())
-    #     btn.move(150, 150)
+class TMainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.init_ui()
+
+    def init_ui(self):
+        """
+
+        :return:
+        """
+        ok_button = QPushButton("OK")
+        cancel_button = QPushButton("Cancel")
+        v_box = QHBoxLayout()
+        v_box.addStretch()
+        v_box.addWidget(ok_button)
+        v_box.addWidget(cancel_button)
+        self.setGeometry(300, 300, 300, 300)
+        self.setLayout(v_box)
+        self.show()
+
+        # vbox.addLayout(okButton, )
+
+
+# def main_menu_create(hwnd):
+#     """
+#
+#     :param hwnd:
+#     :return:
+#     """
+#
+#     btn = QPushButton('Button', hwnd)
+#     btn.clicked.connect(refresh(btn, hwnd))
+#     btn.setToolTip('This is a <b>QPushButton</b> widget')
+#     # btn.resize(btn.sizeHint())
+#     btn.move(50, 50)
+#
+#
+# def refresh(self, hwnd):
+#     """
+#
+#     :return:
+#     """
+#     # if hwnd is not None:
+#     #     btn = QPushButton('Button', hwnd)
+#     #     btn.setToolTip('This is a <b>QPushButton</b> widget 2')
+#     #     btn.resize(btn.sizeHint())
+#     #     btn.move(150, 150)
